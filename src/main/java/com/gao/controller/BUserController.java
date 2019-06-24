59ab1fbbeea8c92693f1bc52384707fb0feefa16
13513094569 <13513094569@163.com>
2019-06-24 21:44:40 +0800

    去看看 我里面写的 todo 注释，和xml 里面给你写的注释, 怎么查看去查查Git分支比较

--- src/main/java/com/gao/controller/BUserController.java
+++ src/main/java/com/gao/controller/BUserController.java
@@ -15,38 +15,36 @@ import com.gao.po.B_User;
 
 @Controller
 public class BUserController {
-       @Resource
-    private B_UserBiz  biz;
-       
-       @RequestMapping("/Main")
-       
-//  @RequestParam(value="userid") int userid 将id传入以便拿到值做结果的查询
-       
-public @ResponseBody B_User getUser(@RequestParam(value="userid") int userid) {
-    	   
-    	 return  biz.getUserById(userid);
-    	   
-}
-	
-       @RequestMapping("/Login")
-       public  @ResponseBody B_User LoginUser(@RequestParam(value="yhm") String yhm,@RequestParam(value="mm") String mm) {
-    	   
-    	       Map map = new HashMap();
-    	       
-    	       map.put("yhm", yhm);
-    	       
-    	       map.put("mm", mm);
-    	       
-    	       B_User  user = biz.CheckLogin(map);
-    	       
-    	       
-    	       return user;
-    	   
-    	   
-    	   
-    	   
-       }
-	
-	
-	
+    @Resource
+    private B_UserBiz biz;
+
+    // TODO: 2019/6/24 去看看 restful 风格的api写法。 
+    @RequestMapping("/Main")
+    @ResponseBody
+    public B_User getUser(@RequestParam(value = "userid") int userid) {
+        return biz.getUserById(userid);
+
+    }
+
+    // TODO: 2019/6/24  所有变量名、方法名 要遵循 "驼峰命名" 规则，去查查！！ 
+    @ResponseBody
+    @RequestMapping("/Login")
+    public B_User LoginUser(@RequestParam(value = "yhm") String yhm, @RequestParam(value = "mm") String mm) {
+
+        // TODO: 2019/6/24 Map<?,?> map = new HashMap(16) 去看看 Map 底层实现。
+        Map map = new HashMap();
+
+        map.put("yhm", yhm);
+
+        map.put("mm", mm);
+
+        B_User user = biz.CheckLogin(map);
+
+
+        return user;
+
+
+    }
+
+
 }
